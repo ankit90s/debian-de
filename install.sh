9e64cf06f1135f3b13cb2e2b27086aba1f468a36
ankit90s <ak7591527@gmail.com>
2022-12-17 23:01:17 +0530

    second commit

--- install.sh
+++ install.sh
@@ -2,16 +2,22 @@
 
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bkp
 sudo cp /home/$USER/debian-gnome/sources.list /etc/apt/sources.list
-sudo apt update && sudo apt -y upgrade
+sudo apt update && sudo apt upgrade -y
 
 # gnome tweaks
 sudo apt install -y gnome-tweaks
 
 # Applications
-sudo apt install -y curl git wget axel pip htop neofetch ranger neovim
+sudo apt install -y curl git wget axel pip htop neofetch ranger rofi neovim
 
 # mpv
 source ~/home/$USER/debian-gnome/mpv.sh
 
 # nerd fonts
 source ~/home/$USER/debian-gnome/nerdfonts.sh
+
+# copy scripts folder
+cp -r /home/$USER/debian-gnome/scripts /home/$USER/
+
+# copy config files
+cp -r /home/$USER/debian-gnome/dotconfig/* /home/$USER/.config/
