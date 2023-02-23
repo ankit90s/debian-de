11da3640d3e659a1a835cb650c23acfc9e96e116
ankit90s <ak7591527@gmail.com>
2023-02-24 00:15:25 +0530

    merged

--- xfce-install.sh
+++ xfce-install.sh
@@ -1,100 +1,92 @@
-#!/usr/bin/env bash
-
-## configure and install minimal xfce desktop environment
-
-## check for sudo/root
-if ! [ $(id -u) = 0 ]; then
-  echo "This script must run with sudo, try again..."
-  exit 1
-fi
+#!/bin/sh bash
 
 # Copy Source file
-mv /etc/apt/sources.list /etc/apt/sources.list.bkp
-cp /home/$USER/debian-de/sources.list /etc/apt/sources.list
-apt update && sudo apt upgrade -y
+sudo mv /etc/sudo apt/sources.list /etc/sudo apt/sources.list.bkp
+sudo cp /home/$USER/debian-de/sources.list /etc/sudo apt/sources.list
+sudo apt update && sudo sudo apt upgrade -y
 
 # Applications and utilities
-apt install -y git pip ranger cmatrix espeak ncdu translate-shell rsync kdeconnect yt-dlp gpick
+sudo apt install -y git pip ranger cmatrix espeak ncdu translate-shell rsync kdeconnect yt-dlp gpick
 
 # Torrent Management
-apt install -y transmission
+sudo apt install -y transmission
 
 # sensors
-apt install -y lm-sensors hddtemp
+sudo apt install -y lm-sensors hddtemp
 
 # Archiving tools
-apt install -y zip unzip
+sudo apt install -y zip unzip
 
 # EXA file list
 # replace default ls command with the following line in .bashrc file
 # alias ls = exa --long --icons --group-directories-first color=always
-apt install -y exa
+sudo apt install -y exa
 
 # search applications
-apt install -y rofi dmenu
+sudo apt install -y rofi dmenu
 
 # System Monitors
-apt install -y conky neofetch htop
+sudo apt install -y conky neofetch htop
 
 # Calculator
-apt install -y gnome-calculator
+sudo apt install -y gnome-calculator
 
 # Download Managers
-apt install -y curl wget axel aria2
+sudo apt install -y curl wget axel aria2
 
 # Network File Tools/System Events
-apt install -y dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends locate mtp-tools
+sudo apt install -y dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends locate mtp-tools
 
-systemctl enable avahi-daemon
-systemctl enable acpid
+sudo systemctl enable avahi-daemon
+sudo systemctl enable acpid
 
 # redshift
-apt install -y redshift
+sudo apt install -y redshift
 
 # Arc theme
-apt install -y arc-theme
+sudo apt install -y arc-theme
 
 # Text editor
-apt install -y neovim geany
+sudo apt install -y neovim geany
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # fonts
-apt install -y fonts-indic fonts-noto-color-emoji fonts-ubuntu fonts-roboto fonts-ubuntu
+sudo apt install -y fonts-indic fonts-noto-color-emoji fonts-ubuntu fonts-roboto fonts-ubuntu
 
 # Install plank dock
-apt install -y plank
+sudo apt install -y plank
 
 # Material Design Icon Fonts
 wget https://github.com/zavoloklom/material-design-iconic-font/releases/download/2.2.0/material-design-iconic-font.zip
-unzip material-design-iconic-font.zip -d /usr/share/fonts/
+sudo unzip material-design-iconic-font.zip -d /usr/share/fonts/
 rm -f material-design-iconic-font.zip
 fc-cache -fv
 
 # Papirus icon
-apt install -y papirus-icon-theme -t bullseye-backports
+sudo apt install -y papirus-icon-theme -t bullseye-backports
 
 # Papirus folder
 wget -qO- https://git.io/papirus-folders-install | sh
 papirus-folders -C yaru --theme Papirus-Dark
 
 # kde apps theme fix
-apt install -y qt5-style-plugins
-cp /home/$USER/debian-de/environment /etc
+sudo apt install -y qt5-style-plugins
+sudo cp /home/$USER/debian-de/environment /etc
 
 # Restricted extras
-apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+sudo apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
 
 # Microsoft fonts
-apt install -y fonts-crosextra-carlito fonts-crosextra-caladea
+sudo apt install -y fonts-crosextra-carlito fonts-crosextra-caladea
 
 # libreoffice install
-apt install -y libreoffice-gtk3 libreoffice-impress libreoffice-calc libreoffice-writer
+sudo apt install -y libreoffice-gtk3 libreoffice-impress libreoffice-calc libreoffice-writer
 
 # libreoffice sifr theme
-curl -s https://raw.githubusercontent.com/rizmut/libreoffice-style-sifr/master/install-sifr.sh | sh
+sudo curl -s https://raw.githubusercontent.com/rizmut/libreoffice-style-sifr/master/install-sifr.sh | sh
 
 ## Xfce4 installation
-apt install -y \
+sudo apt install -y \
     libxfce4ui-utils \
     thunar \
     xfce4-appfinder \
@@ -121,17 +113,17 @@ apt install -y \
     firefox-esr
 
 # Copy xresources
-cp ~/debian-de/Xresources ~/.Xresources
+sudo cp ~/debian-de/Xresources ~/.Xresources
 
 # copy bashrc and bash_aliases
-cp ~/debian-de/bashrc ~/.bashrc
-cp ~/debian-de/bash_aliases ~/.bash_aliases
+sudo cp ~/debian-de/bashrc ~/.bashrc
+sudo cp ~/debian-de/bash_aliases ~/.bash_aliases
 
 # copy config files
-cp -r /home/$USER/debian-de/dotconfig/* /home/$USER/.config/
+sudo cp -r /home/$USER/debian-de/dotconfig/* /home/$USER/.config/
 
 # install slick greeter
-apt install -y slick-greeter lightdm-gtk-greeter-settings lightdm-settings numlockx
+sudo apt install -y slick-greeter lightdm-gtk-greeter-settings lightdm-settings numlockx
 
 echo 
 echo xfce install complete, please reboot and issue 'startx'
