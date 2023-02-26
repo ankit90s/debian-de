2aca58355c5a2301bc7fcb53c735921e3053ea2e
ankit90s <ak7591527@gmail.com>
2023-02-26 14:24:43 +0530

    bug fixes

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,12 @@
-#! /bin/sh
+#! /bin/bash
+
+# check for sudo/root
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
 
 # wallpapers
 wget https://gitlab.com/dwt1/wallpapers/-/archive/master/wallpapers-master.zip
-sudo unzip wallpapers-master.zip -d /usr/share/images/
+unzip wallpapers-master.zip -d /usr/share/images/
 rm wallpapers-master.zip
